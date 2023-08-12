20fe820d239ca6b3ad2b2585eaed0093bc4b9497
Nathan Barry <nathanbarry474@gmail.com>
2023-08-12 13:00:48 -0500

    init

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use std::f64;
+use dioxus::prelude::*;
+use wasm_bindgen::prelude::*;
+
+fn main() {
+    // launch the web app
+    dioxus_web::launch(app);
+}
+
+fn draw() {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    context.begin_path();
+
+    // Draw the outer circle.
+    context
+        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    // Draw the mouth.
+    context.move_to(110.0, 75.0);
+    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
+
+    // Draw the left eye.
+    context.move_to(65.0, 65.0);
+    context
+        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    // Draw the right eye.
+    context.move_to(95.0, 65.0);
+    context
+        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    context.stroke();
+}
+
+fn app(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "Hello, world!"
+            canvas {
+                id: "canvas",
+                width: "200",
+                height: "100",
+                style: "border: 1px solid black",
+            }
+            button {
+                onclick: move |_| draw(),
+                "Click me!"
+            }
+        }
+    })
+}
